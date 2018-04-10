325a90ef002883f4c3904575a6f0d2ad2e5c314b
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2018-04-10 14:40:59 +0530

    Initilized project

---
+++ SystemInfoApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SystemInfoApp
+//
+//  Created by Nitesh Meshram on 4/10/18.
+//  Copyright © 2018 V2Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SystemInfoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SystemInfoApp
+//
+//  Created by Nitesh Meshram on 4/10/18.
+//  Copyright © 2018 V2Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
